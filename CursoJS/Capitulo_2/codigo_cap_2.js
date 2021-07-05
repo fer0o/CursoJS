20b2fdd04a1e8803c5f159aac606f270e1766bd1
fer0o <fer0osennin@gmail.com>
2021-07-05 05:02:18 -0500

    capitulo 2 arrays curso dalto 10 hrs

--- CursoJS/Capitulo_2/codigo_cap_2.js
+++ CursoJS/Capitulo_2/codigo_cap_2.js
@@ -1,1 +1,27 @@
-alert("Hola")
+/**
+ * codigo de ejemplo de arrays
+ * let frutas =["banana", "manzanas", "pera", "mango"];
+document.write(frutas[0])
+ */
+
+let pc ={
+    nombrePc: "fer0oPC",
+    procesador: "intel i7",
+    ram: "16gb",
+    espacio: "500GB"
+};
+let nombrePC = pc["nombrePc"];
+let procesador =pc["procesador"];
+let ram = pc["ram"];
+let discoDuro = pc["espacio"];
+
+frase = `el nombre de mi PS es <b>${nombrePC}</b>  <br>
+el procesador es <b>${procesador}</b> <br> 
+la memoria ram  es de  <b>${ram}</b> <br>
+mientras que la capacidad de disco es de <b>${discoDuro}</b>`
+;
+
+
+document.write(frase);
+
+
