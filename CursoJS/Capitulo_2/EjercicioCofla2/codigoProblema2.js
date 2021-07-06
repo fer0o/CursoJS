6267421a694953c075d5eda638eaf28af076b748
fer0o <fer0osennin@gmail.com>
2021-07-06 04:36:36 -0500

    curso js nivel js dalto, tiempo 3:42  problema 1  cofla resuelto, pero se puede mejorar

--- CursoJS/Capitulo_2/EjercicioCofla2/codigoProblema2.js
+++ CursoJS/Capitulo_2/EjercicioCofla2/codigoProblema2.js
@@ -1,13 +1,30 @@
-let edad = prompt("que edad tienes? ");
-edad = parseInt(edad);
+let entrada_gratis = false;
 
-const verificarEdad = (edad)=>{
-    if(edad <18){
-        document.write("no puedes pasar chaval")
+const verificarCliente = (time)=>{
+    let edad=prompt(" decime tu edad");
+    if(edad >= 18){
+        if( time >=2 && time < 7 && entrada_gratis == false){
+            alert("podes pasar  gratis por que eres la primer persona despues de las 2 AM");
+            entrada_gratis = true;
+        }
+        else{
+            alert(`son las ${time}:00Hs y puedes pasar, pero tienes que pagar la entrada`)
+        }
     }
     else{
-        document.write("pasa y ponete hasta el culo")
+        alert("sos menor de edad, andate a la mierdaa")
     }
 
 }
-verificarEdad(edad)
+verificarCliente(23);
+verificarCliente(24);
+verificarCliente(2);
+verificarCliente(17);
+verificarCliente(2);
+verificarCliente(3);
+verificarCliente(4);
+verificarCliente(9);
+verificarCliente(8);
+verificarCliente(3);
+
+
