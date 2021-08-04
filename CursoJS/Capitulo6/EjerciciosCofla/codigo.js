8f9505b11f68f37cdeb31cd4ee6e15eadfef9022
Fernando Ángel -Medellín Cuevas <you@example.com>
2021-08-04 17:07:15 -0500

    iniciando problema cofla final

--- CursoJS/Capitulo6/EjerciciosCofla/codigo.js
+++ CursoJS/Capitulo6/EjerciciosCofla/codigo.js
@@ -1,1 +1,13 @@
-document.write("ya casi acabamos")
+function crearLlave(nombre, modelo, precio){
+    nombre = `<h2>${nombre}</h2>`;
+    modelo = `<h3>${modelo}</h3>`;
+    precio = `<p>Precio: <b>$${precio}</b></p>`;
+
+    return[nombre,modelo,precio]
+
+}
+//devolver un arreglo pra lo que necesitamos
+
+const llave = crearLlave("Llave 1", "modelo X","33")
+
+document.write(llave[0],llave[1],llave[2])
