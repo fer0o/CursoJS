b360c255538fa54c45d0ce014729f4f355713fc3
Fernando Ángel -Medellín Cuevas <you@example.com>
2021-08-04 03:26:45 -0500

    ultima practica cofla

--- CursoJS/Capitulo6/Leccion/codigo.js
+++ CursoJS/Capitulo6/Leccion/codigo.js
@@ -1,2 +1,13 @@
-const input = document.querySelector(".input-normal")
-document.write(input.className)
+const contenedor = document.querySelector(".contenedor")
+
+const fragmento = document.createDocumentFragment();
+
+
+for(i=0 ; i< 20; i++){
+    const item = document.createElement("LI")
+    item.innerHTML = "Este es un item de la lista"
+    fragmento.appendChild(item)
+}
+
+contenedor.appendChild(fragmento)
+console.log(contenedor)
