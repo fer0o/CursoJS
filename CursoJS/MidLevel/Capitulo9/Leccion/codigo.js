6e4ba51742ff6d5cbafdc7893dec737c1699030f
Fernando Ángel -Medellín Cuevas <you@example.com>
2021-08-10 04:37:20 -0500

    2:23:45

--- CursoJS/MidLevel/Capitulo9/Leccion/codigo.js
+++ CursoJS/MidLevel/Capitulo9/Leccion/codigo.js
@@ -2,14 +2,22 @@ const button = document.querySelector(".button");
 const contenedor = document.querySelector(".contenedor");
 
 
+button.addEventListener('mouseout', (e)=>{
+    alert("di click en el boton")
+    console.log("boton")
+
+});
+/*
 contenedor.addEventListener("click", (e)=>{
-    alert("di click en el target")
+    alert("di click en el contenedor")
     console.log("contenedor");
-})
+},true)
 button.addEventListener("click", (e)=>{
     alert("di click en el boton")
     console.log("boton")
 
 });
+
+*/
 
 
