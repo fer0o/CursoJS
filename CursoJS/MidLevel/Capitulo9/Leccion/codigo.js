fd8df01b5dab02040b002ef522e7f806faffe144
fer0o <fer0osennin@gmail.com>
2021-08-11 20:56:06 -0500

    problema 1 cofla cap 9 3:16:40

--- CursoJS/MidLevel/Capitulo9/Leccion/codigo.js
+++ CursoJS/MidLevel/Capitulo9/Leccion/codigo.js
@@ -1,5 +1,15 @@
+/*
+const inputUser = document.querySelector(".input-user");
+const contenedorUser=document.querySelector(".seleecionado-user")
 const input = document.querySelector(".input-prueba")
 const contenedor = document.querySelector(".seleccionado")
+
+inputUser.addEventListener("select",(e)=>{
+    let start = e.target.selectionStart;
+    let end = e.target.selectionEnd;
+    let textoComplet = inputUser.value;
+    contenedor.textContent = console.log(textoComplet.substring(start,end));
+})
 
 input.addEventListener("select", (e)=>{
     let start = e.target.selectionStart;
@@ -7,3 +17,25 @@ input.addEventListener("select", (e)=>{
     let textoComplet = input.value;
     contenedor.textContent = console.log(textoComplet.substring(start,end));
 })
+*/
+
+const inputUser = document.querySelector(".input-user");
+const contenedorUser=document.querySelector(".seleecionado-user")
+const input = document.querySelector(".input-prueba")
+const contenedor = document.querySelector(".seleccionado")
+
+inputUser.addEventListener("select",(e)=>{
+    let start = e.target.selectionStart;
+    let end = e.target.selectionEnd;
+    let textoComplet = inputUser.value;
+    contenedor.textContent = console.log(textoComplet.substring(start,end));
+})
+
+input.addEventListener("select", (e)=>{
+    let start = e.target.selectionStart;
+    let end = e.target.selectionEnd;
+    let textoComplet = input.value;
+    contenedor.textContent = console.log(textoComplet.substring(start,end));
+})
+
+
