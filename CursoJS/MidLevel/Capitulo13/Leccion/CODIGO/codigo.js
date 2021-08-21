0ef8e2412a13a16abf9ec1f13e2c0069923570a2
Fernando Ángel -Medellín Cuevas <you@example.com>
2021-08-21 02:39:25 -0500

    cap 13 finalizado, codigo respaldo servidor xammp, falta solucion problema cofla

--- CursoJS/MidLevel/Capitulo13/Leccion/CODIGO/codigo.js
+++ CursoJS/MidLevel/Capitulo13/Leccion/CODIGO/codigo.js
@@ -1,23 +1,3 @@
-
-// const request = new XMLHttpRequest();
-// //console.log(request)
-
-// request.addEventListener("load",()=>{
-//     //console.log(request.readyState)
-//     let response;
-//     if(request.status==200) response = request.response
-//     else response = "No se encuentra el archivo"
-//     console.log(response)
-// })
-// request.open("GET","informacion.txt")
-
-// request.send();
-
-// //console.log(request)
-
-
-//------------------------------------------------------------
-//------------------------------------------------------------
 //  let peticion;
 //  if(window.XMLHttpRequest) peticion = new XMLHttpRequest();
 //  else peticion = new ActiveXObject("Microsoft.XMLHTTP");
@@ -58,3 +38,88 @@
 //  }));
 
 //------------------------------fetch-------------------------------------------------------
+
+/*
+fetch ("https://reqres.in/api/unknown/2")
+
+//fetch se usa con promnesas, osea que para acceder a los valores es necesario usar un .then para llamar una promesa
+//por default el fetch tiene el metodo get para obtener informacion de las apis
+//pero se tiene que hacer otra cosa para un metodo post
+
+
+.then(res => res.text())
+.then(res => console.log(JSON.parse(res)))
+console.log()
+*/
+
+
+//-----para metodo post------
+// fetch("https://reqres.in/api/users",{
+//     method:"POST", // se especifica que metodo va a ser, en este caso POST
+//     body: JSON.stringify({                                 //->aqui como se accede como un json hay que deserializar el json con el stringify
+//         "nombre" : "Pamela ",
+//         "apellido": "Beltran",                   //---> Datos
+//     }),
+//     headers:{"Content-type": "application/json"}
+
+// }) //---> se cierra el fetch
+
+
+// .then(res=>res.json())
+// .then(res => console.log(res))
+
+// axios.post("https://reqres.in/api/users",{
+//     "nombre" : "Pamela",
+//     "apellido": "Beltran",
+//     "Instagram" : "bltn.hao"
+// }).then(res=>console.log(res))
+//------------------------------------axios en metodo post
+// axios.post("https://reqres.in/api/users",{
+//      "nombre" : "Pamela",
+//      "apellido": "Hao",
+//     "Instagram" : "fssdfsdfsdf"
+//  }).then(res=>{
+//      console.log(res)
+//  })
+
+//----------------------------------------axios metodo get----------------
+//  axios("informacion.txt")
+//     .then(res=>console.log(res))
+
+//-------async await con axios
+
+const getName = async ()=>{
+    let resultado = await axios("informacion.txt");
+    //let resultado = await resultado.json();
+    let div = document.createElement("DIV")
+    div.innerHTML =resultado.data.nombre;
+    div.classList.add("nombre")
+    document.body.appendChild(div)
+
+}
+
+
+
+const getLastName = async ()=>{
+    let resultado = await axios("informacion.txt");
+    //let resultado = await resultado.json();
+    let div = document.createElement("DIV")
+    div.innerHTML =resultado.data.apellido;
+    div.classList.add("apellido")
+    document.body.appendChild(div)
+}
+
+const getAge = async ()=>{
+    let resultado = await axios("informacion.txt");
+    //let resultado = await resultado.json();
+    let div = document.createElement("DIV")
+    div.innerHTML =resultado.data.edad;
+    div.classList.add("edad")
+    document.body.appendChild(div)
+}
+
+
+
+document.getElementById('getName').addEventListener("click", getName);
+document.getElementById('getLastName').addEventListener("click", getLastName);
+document.getElementById('getAge').addEventListener("click", getAge);
